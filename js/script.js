5d60c30d8b3f3e7d4735e05da3783f8779369f40
Shivam Kajaria <bhivam>
2020-06-10 14:05:46 -0400

    adding javascript

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
+$(document).ready(function(){});
 function testButton() {
   alert("Hello! I am an alert box!!");
-$(document).ready(function(){
-  function addTask() {
-    var text = "<li>hello world</li>";
-    $(".list").after(text);
-  };
-
-  function testButton() {
-    alert("Hello! I am an alert box!!");
-  };
-});
+}
