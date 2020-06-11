0e8d2379330fb16c6589f21eafaa5010e7c93fb1
Shivam Kajaria <bhivam>
2020-06-10 20:01:08 -0400

    Basic Functionality Finished

--- js/script.js
+++ js/script.js
@@ -1,4 +1,40 @@
-$(document).ready(function(){});
-function testButton() {
-  alert("Hello! I am an alert box!!");
+// This is to complete items when they are clicked
+$(".list").delegate("a", 'click', function() {
+    if ($(this).hasClass("checked1")) {
+      $(this).removeClass("checked1");
+    } else {
+      $(this).addClass("checked1");
+    }
+});
+
+$(".list").delegate("list-item-group, .close", "click", function() {
+  $(this).parent().remove();
+});
+
+// This function adds tasks to your list
+function addTask() {
+  itemValue = $(".newItem").val();
+  var item = $('<a href="#" class="list-group-item list-group-item-action"></a>').text(itemValue);
+  var button = $('<button type="button" class="close"></button>');
+  var closeSign = $('<span></span>').text("×");
+  button.append(closeSign);
+  item.append(button);
+  if (itemValue.length < 1) {
+    alert("Please type a task in the box.")
+  } else {
+    $(".list").append(item);
+    $(".newItem").val('');
+  }
+}
+
+// Picks random task from the list
+function pickFromJar() {
+  $(".list").children().removeClass("selected");
+  var itemNumber = getRandomInt($(".list").children().length);
+  $(".list a:nth-child(" + itemNumber + ")").addClass("selected");
+}
+
+// This is for selecting a random integer from 1 to another number
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max)) + 1;
 }
